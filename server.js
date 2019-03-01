5397e3e8c0a2fceb006b7b9e0583ba6812197cdd
crsimpson5 <crsimpson5@gmail.com>
2019-03-01 17:09:42 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// /
+
+// survey
+
+// api/friends
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
